dbd99875bc322f6fd3bb0dbaae45666023e0b969
hbb <zhanglinghan1987@126.com>
2017-09-07 17:32:46 +0800

    branch test

--- chapt03/Primitives/Primitives.cpp
+++ chapt03/Primitives/Primitives.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
 
+    glutSwapBuffers();
 
     return 1;
 }
